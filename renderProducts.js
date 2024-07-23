8344866e62f25543e160f08b05b0024ed56e1446
edsoglez <edsoglez@gmail.com>
2024-07-23 13:38:32 -0600

    added undo add

--- renderProducts.js
+++ renderProducts.js
@@ -20,7 +20,52 @@ window.order = {}
 window.orderArr = []
 window.total = 0
 
+function clearOrder(){
+    order = {}
+    orderArr = []
+    total = 0
+    calcTotal()
+    orderList.innerHTML = ''
+}
+
+function undoAdd(){
+    //check array for latest
+    console.log('latest added',latest);
+    //if in object qty greater than 1, decrease by 1
+    if(order[latest][0] > 1) {
+        order[latest][0]--
+        console.log('minus one: ',order)
+    }
+    //if 1, remove from object
+    else{
+        delete order[latest]
+        console.log('removed',order)
+    }
+    //remove last element from array
+    orderArr.pop()
+    calcTotal()
+    renderOrder()
+}
+
 function renderOrder(){
+    orderList.innerHTML = ''
+
+    Object.entries(order).forEach((itemOrdered)=>{
+        let item = String(itemOrdered[0]).split(' ')[0];
+        let qty = itemOrdered[1][0];
+        let size = String(itemOrdered[0]).split(' ')[1];
+        let price = String(itemOrdered[1][1])
+        
+        orderList.innerHTML +=`
+        <li class="ordered-items">
+            <div style="flex: 5; text-align: left;">${String(item).replaceAll('_',' ')} ${size}</div>                         
+            <div style="flex: 1; text-align: left;">x${qty}</div>
+            <div style="flex: 2; text-align: left;">$ ${price}</div>
+        </li>
+        `
+    })
+    
+    
     orderTotalDisp.innerText = `$ ${total}`
 }
 
@@ -48,23 +93,13 @@ function addToOrder(itemsizeprice){
     }
     
     calcTotal()
-
-    orderList.innerHTML +=`
-    <li class="ordered-items">
-        <div style="flex: 5; text-align: left;">${String(item).replaceAll('_',' ')} ${size}</div>                         
-        <div style="flex: 2; text-align: left;">$ ${price}</div>
-    </li>
-    `
-
-    
-    
+    window.latest = orderArr[orderArr.length-1]
 }
 
 function getSizes(product){
     let Size = product.val().Sizes
     
     window.test = Object.entries(Size)
-    console.log(test)
     let Options = test.map((size,index)=>
         `
         <button class="size-button" id="${product.key+'-'+index}" onclick="addToOrder('${product.key} ${Object.keys(test[index][1])} ${Object.values(Object.values(test)[index][1])[0].price}')">
@@ -80,7 +115,6 @@ get(child(ref(db),`/businesses/${business}/Products/`)).then((Products) => {
         function(product){
             
             let image = Object.values(product.val())[1]
-            console.log('ProductData',image)
             prodList.innerHTML += `
             <div class="product" id="${product.key}-card">
                 <section ondblclick="removeItem('${product.key}')" style="background-color:rgb(200,200,200); background-image: linear-gradient(to top, rgba(255, 255, 255, 0), rgba(240, 240, 240, 1)),url('${image}'); background-size: cover;background-position: center; height:65%; margin: 6px; border-radius: 6px; display: flex; align-content: center; justify-content: center;">
@@ -107,5 +141,7 @@ function removeItem(item){
     }
 }
 
+window.undoAdd = undoAdd
 window.removeItem = removeItem
 window.addToOrder = addToOrder
+window.clearOrder = clearOrder
