774685ee091b9c336746d8fd3b3fe31f1d8a13fc
Eduardo Camacho <57262325+edsoglez@users.noreply.github.com>
2024-11-08 12:57:51 -0600

    header

--- headerRender.js
+++ headerRender.js
@@ -1,7 +1,7 @@
 document.getElementById('header').innerHTML = `
-    <h1>
+    <h1 style="width: 80vw;">
         <img src="fdeicon.png" height="24px" alt="">
-        FDE: <span id="business">${localStorage.getItem('business')}</span></h1>
+        FDE: <span id="business" style="overflow: hidden;">${localStorage.getItem('business')}</span></h1>
     
     <div id="logout" onclick="location.href = 'index.html'">Cerrar Sesión</div>
 `
