424f555305b85550552fbbf14b8beb6e7e678f3d
Eduardo Camacho <57262325+edsoglez@users.noreply.github.com>
2025-01-13 15:42:26 -0600

    asdasd

--- getrecipeitems.js
+++ getrecipeitems.js
@@ -25,7 +25,9 @@ get(child(ref(db),`/businesses/${business}/Items/`)).then((Items) => {
         function(item){
             availItems.innerHTML += `
             <li id="${item.key}" style="background-color: white; padding-inline: 8px; width: auto; display: flex; flex-direction: row; gap: 8px; align-items: center; border: 0px solid rgb(220,220,220); border-radius: 10px;">
-                    <div style="text-align: left; flex-grow: 1; font-weight: bold; padding-left: 4px;">${String(item.key).replaceAll('_',' ')}</div><div style="width: 70px; text-align: right;"><button style="width: 60px; height: 30px; padding:0;">Add+</button></div><div style="text-align: right; padding: 0px; width: 100px;"><input type="text" placeholder="Cantidad" style="height: 16px; width: 80px; border-radius: 6px; border: 0px; text-align: right;"></div>
+                    <div style="text-align: left; flex-grow: 1; font-weight: bold; padding-left: 4px;">${String(item.key).replaceAll('_',' ')}</div>
+                    </div><div style="text-align: right; padding: 0px; width: 100px;"><input type="text" placeholder="Cantidad" style="height: 16px; width: 80px; border-radius: 6px; border: 0px; text-align: right;"></div>
+                    <div style="width: 70px; text-align: right;"><button style="width: 30px; height: 30px; padding:0; padding-bottom:4px">+</button>
             </li>
             `
 
