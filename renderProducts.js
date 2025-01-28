78945a7ff22646f119ec5671cedcfe8d2e914543
Eduardo Camacho <57262325+edsoglez@users.noreply.github.com>
2025-01-28 10:06:13 -0600

    subnav

--- renderProducts.js
+++ renderProducts.js
@@ -269,9 +269,9 @@ function renderOrder(){
         
         orderList.innerHTML +=`
         <li class="ordered-items">
-            <div style="flex: 12; text-align: left;">${String(item).replaceAll('_',' ')} ${size}</div>                         
-            <div style="flex: 2; text-align: left;">x${qty}</div>
-            <div style="flex: 3; text-align: right; padding-right: 10px">$ ${price}</div>
+            <div style="flex: 11; text-align: left;">${String(item).replaceAll('_',' ')} ${size}</div>                         
+            <div style="flex: 1; text-align: left;">x${qty}</div>
+            <div style="flex: 5; text-align: right; padding-right: 10px">$ ${price}</div>
         </li>
         `
     })
