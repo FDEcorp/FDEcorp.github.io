1df08b4c92e489b1d5f9f1e0bf3599302b5508f1
Eduardo Camacho <57262325+edsoglez@users.noreply.github.com>
2025-01-29 16:15:36 -0600

    able to cancel

--- getUsers.js
+++ getUsers.js
@@ -15,23 +15,30 @@ get(child(ref(db),`businesses/${business}/users`)).then((Users)=>{
         UsersList.innerHTML += `
             <div class="item" id="${user.key}-card">
                 <div style="margin: 6px; border-radius: 6px; display: flex; flex-direction: row; gap: 8px; flex: 1">
-                    <div class="wrap" style="flex: 5; font-size: 16px; color: Black; width: 100px; text-align: left; padding: 10px; display: flex; flex-direction: column;">
+                    <div class="wrap" style="flex: 4; font-size: 16px; color: Black; width: 100px; text-align: left; padding: 10px; display: flex; flex-direction: column;">
                         <div>
                             <b>${Users.val().name} ${Users.val().lastName}</b> <br>
-                            ${String(Users.val().email).replaceAll('_', ' ')}
+                            ${String(Users.val().email).replaceAll('_', ' ')}<br>
+                            ${Users.val().stripeSubscriptionId}
                         </div>
                     </div>
-                    <div style="display: flex; align-content: center; align-items: center; gap: 10px;">
-                        <div> 
+                    <div style="display: flex; flex: 2; flex-direction:column; align-content: center; align-items: center; gap: 4px;">
+                        <div style="margin-top:16px; padding:0;"> 
                             Admin    
                         </div>
+                         <div> 
                         <input 
                             onchange="changeAdminStatus('${user.key}', this.checked)" 
                             type="checkbox" 
                             id="${user.key}-check" 
-                            style="margin: 10px; height: 20px; width: 20px; text-align: center; color: black; font-weight: bold; border-radius: 10px;" 
+                            style="margin: 0px; height: 20px; width: 20px; text-align: center; color: black; font-weight: bold; border-radius: 10px;" 
                             ${isAdmin ? 'checked' : ''}
-                        >
+                        ></div>
+                    </div>
+                    <div style="display: flex; flex: 1; align-content: center; align-items: center; gap: 10px;">
+                        <div>
+                        <a href="https://checkout-three-ruddy.vercel.app/cancel-subscription?sub_id=${Users.val().stripeSubscriptionId}&username=${Users.val().user}">Cancel</a>
+                        </div>
                     </div>
                 </div>
             </div>`;
