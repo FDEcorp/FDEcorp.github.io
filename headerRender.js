2315e03405614471e2b01b3fa1adf0d1a8c17ca3
Eduardo Camacho <57262325+edsoglez@users.noreply.github.com>
2025-02-10 09:08:25 -0600

    logout button

--- headerRender.js
+++ headerRender.js
@@ -3,5 +3,5 @@ document.getElementById('header').innerHTML = `
         <img src="fdeicon.png" height="24px" alt="">
         FDE: <span id="business" style="overflow: hidden;">${localStorage.getItem('business')}</span></h1>
     
-    <div id="logout" onclick="location.href = 'index.html'">Cerrar Sesión</div>
+    <div id="logout" onclick="location.href = 'https://checkout-three-ruddy.vercel.app/login'">Cerrar Sesión</div>
 `
