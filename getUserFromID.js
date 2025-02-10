d880de8a31cb570dcef07b588d98680d026d1b82
Eduardo Camacho <57262325+edsoglez@users.noreply.github.com>
2025-02-10 09:33:10 -0600

    percep

--- getUserFromID.js
+++ getUserFromID.js
@@ -20,7 +20,7 @@ get(ref(db,`/temp_users/${tempUser}`)).then((tempUser)=>{
             document.getElementById('header').innerHTML = `
             <h1>
                 <img src="fdeicon.png" height="24px" alt="">
-                FDE: <span id="business" style="overflow: hidden;">${localStorage.getItem('business')}</span></h1>
+                FDE: <span id="business" style="overflow: hidden;">${userdata.val().business}</span></h1>
             
             <div id="logout" onclick="location.href = 'https://checkout-three-ruddy.vercel.app/login'">Cerrar Sesión</div>
         `
