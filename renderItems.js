fe2446fb8b1a5ceb195297c89ce8a4b5c63211d8
Eduardo Camacho <57262325+edsoglez@users.noreply.github.com>
2025-06-14 08:20:40 -0600

    unknown to 0s

--- renderItems.js
+++ renderItems.js
@@ -106,7 +106,7 @@ function renderItems(filter = 'all',productSearch=false){
                     if((localStorage.getItem('resumen')=="true" && item.val().orderQty > 0)||localStorage.getItem('resumen')=="false"){
                         console.log("resumen: ",localStorage.getItem('resumen'))
                         console.log(item.val().orderQty,item.val().packPrice)
-                        orderTotal.innerHTML = Math.round(Number(orderTotal.innerHTML)+ Number(item.val().orderQty)*Number(item.val().packPrice)).toFixed(2)
+                        orderTotal.innerHTML = Math.round(Number(orderTotal.innerHTML)+ Number(item.val().orderQty||0)*Number(item.val().packPrice)||0).toFixed(2)
                         prodList.innerHTML += `
                     <div class="item" id="${item.key}-card" style="background-color: ${item.val().stock>=item.val().minStock?'var(--primary-base-light)':'rgb(255, 238, 163);'};">
                         <div ondblclick="editProd('${item.key}')" style="margin: 6px; border-radius: 6px; display: flex; flex-direction: row; gap: 8px; flex: 1">
