226af5943006886dd77c2ee9c5d557a478e14fcd
Eduardo Camacho <57262325+edsoglez@users.noreply.github.com>
2025-07-08 08:29:47 -0600

    dsadsa

--- headerRender.js
+++ headerRender.js
@@ -5,3 +5,11 @@ document.getElementById('header').innerHTML = `
     
     <div id="logout" onclick="location.href = 'https://checkout-three-ruddy.vercel.app/login'">Cerrar Sesión</div>
 `
+window.addEventListener("scroll", function () {
+    let header = document.getElementById("header");
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    
+    header.style.background = `rgba(0,0,0,${1 - Math.min(scrollTop / 500,0.4)})`
+
+
+ });
