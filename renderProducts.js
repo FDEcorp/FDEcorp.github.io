a20596c209c30adb4f31f33c5bc6c4b3ecc47efb
Eduardo Camacho <57262325+edsoglez@users.noreply.github.com>
2026-04-12 22:56:49 -0600

    sadas

--- renderProducts.js
+++ renderProducts.js
@@ -585,7 +585,7 @@ function renderItems(filter = 'all',productSearch=false){
                                             </svg>
                                             Editar Producto</p>
 
-                                            <p style="margin: 0; padding: 10px; display:flex; gap: 8px; padding-inline:8px; text-align: left; border-radius: 4px; background-color: #f8f8f8; border: 0px solid #bbb; margin-top: 8px; box-shadow: 0px 2px 4px rgba(124,124,124,0.2);" onclick="itemHistory('${product.key}')">
+                                            <p style="margin: 0; padding: 10px; display:flex; gap: 8px; padding-inline:8px; text-align: left; border-radius: 4px; background-color: #f8f8f8; border: 0px solid #bbb; margin-top: 8px; box-shadow: 0px 2px 4px rgba(124,124,124,0.2);" onclick="viewSalesHistory('${product.key}')">
                                             <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                 <path d="M22 7L14.1314 14.8686C13.7354 15.2646 13.5373 15.4627 13.309 15.5368C13.1082 15.6021 12.8918 15.6021 12.691 15.5368C12.4627 15.4627 12.2646 15.2646 11.8686 14.8686L9.13137 12.1314C8.73535 11.7354 8.53735 11.5373 8.30902 11.4632C8.10817 11.3979 7.89183 11.3979 7.69098 11.4632C7.46265 11.5373 7.26465 11.7354 6.86863 12.1314L2 17M22 7H15M22 7V14" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                                             </svg>
@@ -951,6 +951,11 @@ function hideItemMenu(item){
 }
 
 window.itemHistory = itemHistory;
+
+window.viewSalesHistory = viewSalesHistory;
+function viewSalesHistory(prod){
+    location.href = 'salesHistory.html?prod=' + prod;
+}
 
 //logica se usara en pagina dedicada a mostrar el historial de ventas de un producto, con filtros por fecha y posibilidad de mostrar graficos de ventas por dia/semana/mes/ano
 function itemHistory(item){
